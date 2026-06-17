f2c7a5c7d73ffcc51ba52579ce17fcbb2544b8b6
NguyennHieuu <nguyenhieugp2005@gmail.com>
2026-06-17 19:47:52 +0700

    Cấu trúc lại dự án: Đưa mã nguồn vào folder java và sửa lỗi vị trí .git

---
+++ Java/src/main/java/com/nhom10/ooad/quanlysinhvien/View/GiaoDienDangNhap.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nhom10.ooad.quanlysinhvien.View;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class GiaoDienDangNhap {
+    
+}
